872de246e35c3ea9b56b766602e8f878175dcfde
Thriven Komarr <thrivenkomarr@gmail.com>
2026-03-05 05:48:29 -0800

    routes

--- index.js
+++ index.js
@@ -2,36 +2,12 @@ const express = require('express');
 const app = express();
 let port = 4000;
 
-app.use((req, res, next) => {
-    console.log(`${req.method} request made to ${req.url}`);
-    next(); // Move to next middleware/route
-});
+const productsRouter = require('./routes/products');
+const categoriesRouter = require('./routes/categories');
 
-app.use(express.json());
 
-// GET /products
-app.get('/products', (req, res) => {
-    res.status(200).send("Here is the list of all products.");
-});
-
-// POST /products
-app.post('/products', (req, res) => {
-    res.status(201).send("A new product has been added.");
-});
-
-// GET /categories
-app.get('/categories', (req, res) => {
-    res.status(200).send("Here is the list of all categories.");
-});
-
-// POST /categories
-app.post('/categories', (req, res) => {
-    res.status(201).send("A new category has been created.");
-});
-
-app.use((req, res) => {
-    res.status(404).send('<h1>404 - Page Not Found</h1>');
-});
+app.use('/products', productsRouter);
+app.use('/categories', categoriesRouter);
 
 
 app.listen(port, () => {
