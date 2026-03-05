cde0c005fb35e61dd85e86e1fb76a8f7aade356e
Thriven Komarr <thrivenkomarr@gmail.com>
2026-03-05 01:24:29 -0800

    unroute

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const app = express();
-let port = 3000;
+let port = 4000;
 
 app.use(express.json());
 
-/* ---------------- ROUTES ---------------- */
-
-// GET /orders
-app.get('/orders', (req, res) => {
-    res.send("Here is the list of all orders.");
+// GET /products
+app.get('/products', (req, res) => {
+    res.status(200).send("Here is the list of all products.");
 });
 
-// POST /orders
-app.post('/orders', (req, res) => {
-    res.send("A new order has been created.");
+// POST /products
+app.post('/products', (req, res) => {
+    res.status(201).send("A new product has been added.");
 });
 
-// GET /users
-app.get('/users', (req, res) => {
-    res.send("Here is the list of all users.");
+// GET /categories
+app.get('/categories', (req, res) => {
+    res.status(200).send("Here is the list of all categories.");
 });
 
-// POST /users
-app.post('/users', (req, res) => {
-    res.send("A new user has been added.");
+// POST /categories
+app.post('/categories', (req, res) => {
+    res.status(201).send("A new category has been created.");
 });
+
+app.use((req, res) => {
+    res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
