f056f919ee461314236f9e72d35b50185854d2d0
Thriven Komarr <thrivenkomarr@gmail.com>
2026-03-04 23:07:18 -0800

    task2

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
-app.use((req,res,next)=>{
-    console.log("Server is up and running on port 3000! Ready to handle requests.");
-    res.send("<h1>Hello from Express</h1>");
-    // next();
+let port = 3000;
+
+app.use((req, res, next) => {
+    req.user = "Guest";
+    next();
 });
-// app.use((req,res,next)=>{
-//     console.log("This is a middleware 2");
-    
-// }); 
-app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.send("Server is running 🚀");
+});
+
+app.get('/welcome', (req, res) => {
+    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
